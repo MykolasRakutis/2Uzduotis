7c22304812f5b5d1a641769e5e3962a346dd5bae
MykolasRakutis <Mykolas.Rakutis1@gmail.com>
2025-09-24 14:33:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,68 +3,75 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::vector;
-using std::setw;
-using std::left;
-using std::right;
 
 struct Studentas
 {
     string vardas;
     string pavarde;
-    vector <int> ndpaz;
+    vector<int> ndpaz;
     int egzrez;
     double galutinis_vid;
     double galutinis_med;
-
 };
+
 double MedSkaiciavimas(vector<int> paz)
 {
     sort(paz.begin(), paz.end());
-
     int pazsk = paz.size();
     if (pazsk == 0) return 0;
 
     if (pazsk % 2 == 0)
-    {
         return (paz[pazsk / 2 - 1] + paz[pazsk / 2]) / 2.0;
-    } else {
+    else
         return paz[pazsk / 2];
-    }
 }
-
 
 Studentas Stud_ivestis(int nr)
 {
-    int n, laik_paz, sum = 0;
+    int laik_paz, sum = 0;
+    string eilute;
     Studentas pirmas;
+
     cout << "\nIveskite " << nr << "-ojo studento duomenis" << endl;
-    cout << "Vardas ";
+    cout << "Vardas: ";
     cin >> pirmas.vardas;
     cout << "Pavarde: ";
     cin >> pirmas.pavarde;
-    cout << "Kiek namu darbu jis turi? ";
-    cin >> n;
 
-    for (int a = 0; a < n; a++)
+    cin.ignore(1000, '\n');
+
+    cout << "Iveskite namu darbu pazymius (baigti tuscia eilute):" << endl;
+    while (true)
     {
-        cout << a + 1 << ": ";
-        cin >> laik_paz;
-        pirmas.ndpaz.push_back(laik_paz);
-        sum += laik_paz;
+        getline(cin, eilute);
+        if (eilute.empty()) break;
+
+        stringstream ss(eilute);
+        while (ss >> laik_paz)
+        {
+            pirmas.ndpaz.push_back(laik_paz);
+            sum += laik_paz;
+        }
     }
 
     cout << "Iveskite egzamino rezultata: ";
     cin >> pirmas.egzrez;
-    pirmas.galutinis_vid = double(sum) / double(n) * 0.4 + pirmas.egzrez * 0.6;
+
+    int n = pirmas.ndpaz.size();
+    double vid;
+    if (n > 0)
+        vid = double(sum) / n;
+    else
+        vid = 0;
+
+    pirmas.galutinis_vid = vid * 0.4 + pirmas.egzrez * 0.6;
+
     double med = MedSkaiciavimas(pirmas.ndpaz);
     pirmas.galutinis_med = med * 0.4 + pirmas.egzrez * 0.6;
+
     return pirmas;
 }
 
@@ -72,13 +79,15 @@ int main()
 {
     cout << "Laba diena" << endl;
     vector<Studentas> Grupe;
-    cout << "Kiek studentu grupeje?: ";
     int m;
-    cin >> m;
 
-    for (auto z = 0; z < m; z++)
+    cout << "Kiek studentu grupeje?: ";
+    cin >> m;
+    cin.ignore(1000, '\n');
+
+    for (int z = 0; z < m; z++)
     {
-        Grupe.push_back(Stud_ivestis(z+1));
+        Grupe.push_back(Stud_ivestis(z + 1));
     }
 
     cout << "\nStudento informacija:" << endl;
@@ -86,9 +95,10 @@ int main()
          << "|" << left << setw(20) << "Pavarde"
          << "|" << left << setw(15) << "Galutinis(vid)"
          << "|" << left << setw(15) << "Galutinis(med)" << endl;
-    cout << string(65, '-') << endl;
+    cout << string(70, '-') << endl;
 
-    for (const auto &Studentas : Grupe) {
+    for (const auto &Studentas : Grupe)
+    {
         cout << left << setw(15) << Studentas.vardas
              << "|" << left << setw(20) << Studentas.pavarde
              << "|" << left << setw(15) << fixed << setprecision(2) << Studentas.galutinis_vid
